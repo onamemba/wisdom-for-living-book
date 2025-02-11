838b9ae39b62a18a505bcb3cf2c069d1c1baaefb
Francis <francis@al.co.za>
2025-02-11 04:45:53 -0600

    url books

--- src/data/books.ts
+++ src/data/books.ts
@@ -6,8 +6,7 @@ export const books = [
     month: 'January',
     year: '2025',
     coverUrl: '/images/wisdom_for_living_januray_2025.jpg',
-    pdfUrl:
-      'https://jftczcnmuruycztwpqlb.supabase.co/storage/v1/object/public/books/wisdom_for_living_januray_2025.pdf?t=2025-01-13T04%3A14%3A18.952Z',
+    pdfUrl:'/books/wisdom_for_living_januray_2025.pdf',
     likes: 124,
     comments: 45,
   },
@@ -18,8 +17,7 @@ export const books = [
     month: 'February',
     year: '2025',
     coverUrl: '/images/wisdom_for_living_february_2025.jpg',
-    pdfUrl:
-      'https://jftczcnmuruycztwpqlb.supabase.co/storage/v1/object/public/books//wisdom_for_living_february_2025.pdf',
+    pdfUrl:'/books/wisdom_for_living_february_2025.pdf',
     likes: 98,
     comments: 32,
   },
